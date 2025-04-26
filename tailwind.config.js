483b0569c89fe8ca97496fc2cd1fcd3e0542f5f9
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-26 19:40:18 +0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#222831",
+        foreground: "#393E46",
+        primary: "#00ADB5",
+        muted: "#EEEEEE",
+      },
+    },
+  },
+  plugins: [],
+};
